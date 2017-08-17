041f5770f447166f222ffe8a3f8915802359b847
mingpen <1047404778@qq.com>
2017-08-17 16:24:51 +0800

    不支持 注释

--- conf.go
+++ conf.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -51,13 +50,13 @@ func (o *Conf) OpenConf(confFile string) error {
 func (o *Conf) SetConf(data []byte) error {
 	// 替换每行以 / 开头的内容为空
 	// 去掉json中的注释
-	reg, err := regexp.Compile("[ *|\\t*]// .*")
-	if err != nil {
-		return err
-	}
-	d := reg.ReplaceAll(data, nil)
+	//reg, err := regexp.Compile("[ *|\\t*]// .*")
+	//if err != nil {
+	//	return err
+	//}
+	//d := reg.ReplaceAll(data, nil)
 
-	if err := json.Unmarshal(d, &o.j); err != nil {
+	if err := json.Unmarshal(data, &o.j); err != nil {
 		return err
 	}
 	return nil
